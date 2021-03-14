5b791572445df56904e83ae7b33316b0745551cd
jc32101 <jc32101@gmail.com>
2021-03-14 19:40:07 +0000

    bonus

--- test.c
+++ test.c
@@ -1,30 +1,60 @@
-/******************************************************
-* File: frame.c
-*
-* This is the frame struct implementation
-*
-******************************************************/
+// /******************************************************
+// * File: frame.c
+// *
+// * This is the frame struct implementation
+// *
+// ******************************************************/
 #include "frame.h"
 #include <stdlib.h>
+#define LEN 1200
+//_GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
-#define MAX_BUF 1024
+
+#include <fcntl.h>
+#include <unistd.h>
+
+// static void load_frame(frame_t *f, char *path){
+// 	FILE * fp;
+//   char * line = NULL; 
+// 	fp = fopen(path, "r");
+// 	size_t len = 0;
+//   size_t read;
+
+//   if (fp == NULL){
+//     exit(EXIT_FAILURE);
+//   }
+
+//   read = getdelim(&line, &len, EOF, fp);
+// 	f->content = (char*)realloc(f->content, len * sizeof(char));
+// 	strcpy(f->content,line);
+// 	f->content[read] = '\0';
+// 	fclose(fp);
+// 	f->rep_counter = (int)f->content[0];
+//   return;
+// }
+
+
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+-----------------------------------------------------------------
 
 static void load_frame(frame_t *f, char *path){
-  char buf[MAX_BUF];
-  FILE* fp;
-  fp = fopen(path, "r");
+  // char *tokenize, *savePtr;
+  char fileLine [LEN];
+  FILE *fd = fopen(path, "r");
   int m, i;
-  i=0;
-  // int index = 0;
 
-  if (fp == NULL){
-    exit(EXIT_FAILURE);
+  if(fd == NULL){
+    perror("ERROR OPENING FILE");
   }
+  
+  printf("Continue to fgets\n");
+  
+  while(fgets(fileLine, LEN-2, fd) != NULL){
 
-  f->content = (char*)malloc(sizeof(char)*)
-  while(fgets(buf, MAX_BUF, fp)){
-    char *s = (char*)malloc(sizeof(strlen(buf)+2));
+    char *s = (char*)malloc(sizeof(strlen(fileLine)+2));
     for(m = 0; m < strlen(buf); m++){
       *(s+m) = *(buf+m);
     }
@@ -35,22 +65,28 @@ static void load_frame(frame_t *f, char *path){
       i++;
     }
 
-    frame_set(f, i, s);
 
-    // f->content[index] = *s;
-    // index++;
+    printf("While loop --->\n");
+    f->content = (char*)realloc(f->content, strlen(fileLine) * sizeof(char));
+    printf("realloced--->\n");
+    strcat(f->content,fileLine);
+    printf("concat ----> \n content: \n%s\n", f->content);
+
 
   }
-  fclose(fp);
+  fclose(fd);
+  
+  f->rep_counter = (int)f->content[0];
 	return;
 }
+
 
 void frame_set(frame_t* f,  int loc, char* s){
 	if((loc < strlen(f->content)) && (loc >= 0)) {
    	f->content[loc] = *s;
   } 
   else {
-    f->content = (char*) realloc(f->content, sizeof(char)*(loc+1));
+    f->content = (char*) realloc(f->content, sizeof(char)*strlen(s));
 
     for(int i = strlen(f->content); i < loc+1; i++)
 		  f->content[i] = '\0';
@@ -58,14 +94,11 @@ void frame_set(frame_t* f,  int loc, char* s){
 	}
 }
 
-// void frame_cat(frame_t *f, char *s2) {
-// 	/* YOUR CODE HERE*/
-//   size_t j;
-  
-//   for(j = 0; *(s2+j) != '\0'; j++) j++;
-//   f->content = (char *)realloc(f->content, sizeof(char)*(sizeof(f->content) + j));
-//   strcat(f->content, s2);
-// }
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
 
 frame_t * frame_new(char *pathname, int id) {
 	frame_t * f = (frame_t *) malloc(sizeof(frame_t));
@@ -73,7 +106,11 @@ frame_t * frame_new(char *pathname, int id) {
 		perror("OOM, alloc failed\n");
 		return 0;
 	}
+	f->content = (char *)malloc(sizeof(char));
+	f->rep_counter = 0;
 	f->id = id;
+      printf("Continue to load_frame");
+
 	load_frame(f, pathname);
 	return f;
 }
@@ -92,4 +129,3 @@ int frame_get_rep_counter(frame_t * f){
 int frame_get_id(frame_t *f){
 	return f-> id;
 }
-
