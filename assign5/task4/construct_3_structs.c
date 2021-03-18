81a39cad27aba83e73dc1b5cdabe777b6792c474
jc32101 <jc32101@gmail.com>
2021-03-18 13:59:25 +0000

    update

--- assign5/task4/construct_3_structs.c
+++ assign5/task4/construct_3_structs.c
@@ -12,7 +12,33 @@ node_t * setup() {
     //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
     //Dereference each pointer to access the .next field in its pointee, 
     //and use pointer assignment to set the .next field to point to the appropriate Node. 
-    
+    node_t *node1, *node2, *node3;
+
+    node1 = (node_t *)malloc(sizeof(node_t *)*3);
+    node2 = (node_t *)malloc(sizeof(node_t *)*3);
+    node3 = (node_t *)malloc(sizeof(node_t *)*3);
+
+    node1->value = (char*)malloc(sizeof(char *)*6);
+    node2->value = (char*)malloc(sizeof(char *)*6);
+    node3->value = (char*)malloc(sizeof(char *)*5);
+
+    for(int i = 0; i < 6; i++){
+        node1->value[i] = "hello\0"[i];
+    }
+
+    for(int i = 0; i < 6; i++){
+        node2->value[i] = "there\0"[i];   
+    }
+
+    for(int i = 0; i < 5; i++){
+        node3->value[i] = "prof\0"[i];   
+    }
+
+    head->next = node1;
+    head->next->next = node2;
+    head->next->next->next = node3;
+    head->next->next->next->next = NULL;
+
     return head;
 }
 
@@ -20,6 +46,15 @@ void teardown(/*what parameter?*/) {
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
+    if(head->next->next->next != node3){
+      free(node1);
+      free(node2);
+      free(node3);
+    }else{
+        //free(node1->value);
+        //free(node2->value);
+        //free(node3>value);
+    }
 }
 
 void dump_all(node_t*);
