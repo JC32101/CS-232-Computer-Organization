569d978876a4783756d83d3dff2d90947da178d6
jc32101 <jc32101@gmail.com>
2021-03-29 08:06:44 +0000

    updates

--- assign6/task5/labyrinth/Main.cpp
+++ assign6/task5/labyrinth/Main.cpp
@@ -10,13 +10,13 @@ int main() {
      * it's been initialized.
      */
   	std::string name, pathOutOfNormalMaze;
-	std::getline (std::cin, name);
-	std::getline (std::cin, pathOutOfNormalMaze);
+  	std::getline (std::cin, name);
+	  std::getline (std::cin, pathOutOfNormalMaze);
 
     MazeCell* startLocation = mazeFor(name);
     
     /* Set a breakpoint here to explore your maze! */
-    if (isPathToFreedom(startLocation, pathOutOfNormalMaze)) {
+    if (isPathToFreedom(startLocation,pathOutOfNormalMaze)) {
         std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
     } else {
         std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
